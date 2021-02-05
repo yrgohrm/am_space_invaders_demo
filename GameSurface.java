6f3e404c8ddb9cb4eb86a6d118ed87740b7d84ae
henrikbuller <henbul0928@goteborg.skola.se>
2021-02-05 10:58:25 +0100

    Reworked aliens to green pipes with random heights.
    Replaced all occurences of alien with pipe, removed addAlien method and
    replaced it with addPipe.

--- GameSurface.java
+++ GameSurface.java
@@ -16,143 +16,159 @@ import javax.swing.Timer;
 
 /**
  * A simple panel with a space invaders "game" in it. This is just to
- * demonstrate the bare minimum of stuff than can be done drawing on
- * a panel. This is by no means very good code.
+ * demonstrate the bare minimum of stuff than can be done drawing on a panel.
+ * This is by no means very good code.
  * 
  */
 public class GameSurface extends JPanel implements ActionListener, KeyListener {
-    private static final long serialVersionUID = 6260582674762246325L;
+	private static final long serialVersionUID = 6260582674762246325L;
 
-    private boolean gameOver;
-    private Timer timer;
-    private List<Rectangle> aliens;
-    private Rectangle spaceShip;
+	private boolean gameOver;
+	private Timer timer;
+	private ArrayList<Rectangle> pipes;
+	private Rectangle spaceShip;
 
-    public GameSurface(final int width, final int height) {
-        this.gameOver = false;
-        this.aliens = new ArrayList<>();
+	public GameSurface(final int width, final int height) {
+		this.gameOver = false;
+		this.pipes = new ArrayList<>();
 
-        for (int i = 0; i < 5; ++i) {
-            addAlien(width, height);
-        }
+		
+			addPipe(width, height);
+		
 
-        this.spaceShip = new Rectangle(20, width/2-15, 30, 20);
+		this.spaceShip = new Rectangle(20, width / 2 - 15, 30, 20);
 
-        this.timer = new Timer(20, this);
-        this.timer.start();
-    }
+		this.timer = new Timer(20, this);
+		this.timer.start();
+	}
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        repaint(g);
-    }
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		repaint(g);
+	}
 
-    private void addAlien(final int width, final int height) {
-        int x = ThreadLocalRandom.current().nextInt(width / 2, width - 30);
-        int y = ThreadLocalRandom.current().nextInt(20, height - 30);
-        aliens.add(new Rectangle(x, y, 10, 10));
-    }
+	private void addPipe(final int width, final int height) {
+		int randomHeight = ThreadLocalRandom.current().nextInt(height / 2);
+		int gap = 80;
 
-    /**
-     * Call this method when the graphics needs to be repainted
-     * on the graphics surface.
-     * 
-     * @param g the graphics to paint on
-     */
-    private void repaint(Graphics g) {
-        final Dimension d = this.getSize();
+		int x = width;
+		int y = 0; 
 
-        if (gameOver) {
-            g.setColor(Color.red);
-            g.fillRect(0, 0, d.width, d.height);    
-            g.setColor(Color.black);
-            g.setFont(new Font("Arial", Font.BOLD, 48));
-            g.drawString("Game over!", 20, d.width/2-24);
-            return;
-        }
+		int x2 = width;
+		int y2 = height - randomHeight;
 
-        // fill the background
-        g.setColor(Color.cyan);
-        g.fillRect(0, 0, d.width, d.height);
+		int topPipeHeight = height - (randomHeight + gap);
+		
+		// top pipe
+		pipes.add(new Rectangle(x, y, 50, topPipeHeight));
+		
+		System.out.println("top pipe:\t" + "x: " + x + "\ty: "+ y + "\theight: "+ topPipeHeight);
+		// bottom pipe
+		pipes.add(new Rectangle(x2, y2, 50, randomHeight));
+		System.out.println("bottom pipe:\t" + "x:" + x2 + "\ty:"+ y2 + "\theight:"+ randomHeight);
 
-        // draw the aliens
-        for (Rectangle alien : aliens) {
-            g.setColor(Color.red);
-            g.fillRect(alien.x, alien.y, alien.width, alien.height);
-        }
+	}
 
-        // draw the space ship
-        g.setColor(Color.black);
-        g.fillRect(spaceShip.x, spaceShip.y, spaceShip.width, spaceShip.height);
-    }
+	/**
+	 * Call this method when the graphics needs to be repainted on the graphics
+	 * surface.
+	 * 
+	 * @param g the graphics to paint on
+	 */
+	private void repaint(Graphics g) {
+		final Dimension d = this.getSize();
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // this will trigger on the timer event
-        // if the game is not over yet it will
-        // update the positions of all aliens
-        // and check for collision with the space ship
+		if (gameOver) {
+			g.setColor(Color.red);
+			g.fillRect(0, 0, d.width, d.height);
+			g.setColor(Color.black);
+			g.setFont(new Font("Arial", Font.BOLD, 48));
+			g.drawString("Game over!", 20, d.width / 2 - 24);
+			return;
+		}
 
-        if (gameOver) {
-            timer.stop();
-            return;
-        }
+		// fill the background
+		g.setColor(Color.cyan);
+		g.fillRect(0, 0, d.width, d.height);
 
-        final List<Rectangle> toRemove = new ArrayList<>();
+		// draw the pipes
+		for (Rectangle pipe : pipes) {
+			g.setColor(Color.green);
+			g.fillRect(pipe.x, pipe.y, pipe.width, pipe.height);
+		}
 
-        for (Rectangle alien : aliens) {
-            alien.translate(-1, 0);
-            if (alien.x + alien.width < 0) {
-                // we add to another list and remove later
-                // to avoid concurrent modification in a for-each loop
-                toRemove.add(alien);
-            }
+		// draw the space ship
+		g.setColor(Color.black);
+		g.fillRect(spaceShip.x, spaceShip.y, spaceShip.width, spaceShip.height);
+	}
 
-            if (alien.intersects(spaceShip)) {
-                gameOver = true;
-            }
-        }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// this will trigger on the timer event
+		// if the game is not over yet it will
+		// update the positions of all aliens
+		// and check for collision with the space ship
 
-        aliens.removeAll(toRemove);
+		if (gameOver) {
+			timer.stop();
+			return;
+		}
 
-        // add new aliens for every one that was removed
-        for (int i = 0; i < toRemove.size(); ++i) {
-            Dimension d = getSize();
-            addAlien(d.width, d.height);
-        }
+		final List<Rectangle> toRemove = new ArrayList<>();
 
-        this.repaint();
-    }
+		for (Rectangle pipe : pipes) {
+			pipe.translate(-1, 0);
+			if (pipe.x + pipe.width < 0) {
+				// we add to another list and remove later
+				// to avoid concurrent modification in a for-each loop
+				toRemove.add(pipe);
+			}
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // this event triggers when we release a key and then
-        // we will move the space ship if the game is not over yet
+			if (pipe.intersects(spaceShip)) {
+				gameOver = true;
+			}
+		}
 
-        if (gameOver) {
-            return;
-        }
+		pipes.removeAll(toRemove);
 
-        final int minHeight = 10;
-        final int maxHeight = this.getSize().height - spaceShip.height - 10;
-        final int kc = e.getKeyCode();
+		// add new aliens for every one that was removed
 
-        if (kc == KeyEvent.VK_UP && spaceShip.y > minHeight) {
-            spaceShip.translate(0, -10);
-        }
-        else if (kc == KeyEvent.VK_DOWN && spaceShip.y < maxHeight) {
-            spaceShip.translate(0, 10);
-        }
-    }
-    
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // do nothing
-    }
+		for (int i = 0; i < toRemove.size(); ++i) {
+			Dimension d = getSize();
+			addPipe(d.width, d.height);
+		}
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        // do nothing
-    }
+		this.repaint();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// this event triggers when we release a key and then
+		// we will move the space ship if the game is not over yet
+
+		if (gameOver) {
+			return;
+		}
+
+		final int minHeight = 10;
+		final int maxHeight = this.getSize().height - spaceShip.height - 10;
+		final int kc = e.getKeyCode();
+
+		if (kc == KeyEvent.VK_UP && spaceShip.y > minHeight) {
+			spaceShip.translate(0, -10);
+		} else if (kc == KeyEvent.VK_DOWN && spaceShip.y < maxHeight) {
+			spaceShip.translate(0, 10);
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// do nothing
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// do nothing
+	}
 }
