0269c93495d279be8a3ba36a08a4ddd71564c48f
Thomas Haraldsson <thohar2001@skola.goteborg.se>
2021-02-11 11:24:19 +0100

    Added highscore and made it possible to restart game
    Added a highscore variable. Code now checks if current score is higher than highscore, if it is then highscore will be set to the current score. On death, current score and highscore will show on screen.
    
    Made it possible to start a new round when game is over. Press space bar to start new game.

--- GameSurface.java
+++ GameSurface.java
@@ -11,10 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
 /**
  * A simple panel with a space invaders "game" in it. This is just to
- * demonstrate the bare minimum of stuff than can be done drawing on
- * a panel. This is by no means very good code.
+ * demonstrate the bare minimum of stuff than can be done drawing on a panel.
+ * This is by no means very good code.
  * 
  */
 public class GameSurface extends JPanel implements ActionListener, KeyListener {
@@ -22,22 +23,29 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
     private boolean gameOver;
     private Timer timer;
     private List<WarpPipes> pipeList; // used to be: private List<Rectangle> aliens;
-    private Rectangle spaceShip;
+    private Rectangle bird;
     private int jumpRemaining;
     private int frames;
     private int gravity;
-    private int score;
+    private int currentScore;
+    private int width;
+    private int height;
+    private int highscore = 0;
 
     public GameSurface(final int width, final int height) {
+        this.width = width;
+        this.height = height;
+
         this.gameOver = false;
         this.pipeList = new ArrayList<>();
-        
+
         pipeList.add(new WarpPipes(width, height));
 
-        this.spaceShip = new Rectangle(20, width/2-15, 30, 20);
+        this.bird = new Rectangle(20, width / 2 - 15, 30, 20);
         this.timer = new Timer(2, this);
         this.timer.start();
     }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -45,8 +53,8 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
     }
 
     /**
-     * Call this method when the graphics needs to be repainted
-     * on the graphics surface.
+     * Call this method when the graphics needs to be repainted on the graphics
+     * surface.
      * 
      * @param g the graphics to paint on
      */
@@ -54,10 +62,11 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
         final Dimension d = this.getSize();
         if (gameOver) {
             g.setColor(Color.red);
-            g.fillRect(0, 0, d.width, d.height);    
+            g.fillRect(0, 0, d.width, d.height);
             g.setColor(Color.black);
             g.setFont(new Font("Arial", Font.BOLD, 48));
-            g.drawString("Game over!" + " Your score: " + score, 20, d.width/2-24);
+            g.drawString("Game over!" + " Your score: " + currentScore, 20, d.width / 2 - 24);
+            g.drawString("Highscore: " + highscore, 20, d.width / 2 + 24);
             return;
         }
         // fill the background
@@ -69,29 +78,30 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
         }
         // draw the space ship
         g.setColor(Color.yellow);
-        g.fillRect(spaceShip.x, spaceShip.y, spaceShip.width, spaceShip.height);
+        g.fillRect(bird.x, bird.y, bird.width, bird.height);
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         // We count the frames because we want to trigger
         // certain events after a specific amount of frames
         frames++;
 
-        // This will make the game end when you hit a 
+        // This will make the game end when you hit a
         // warp pipe.
 
-        //TODO: Show score and highscore when game is over
-        //TODO: Make it possible to start a new round again
+        // TODO: Show currentScore and highscore when game is over
+        // TODO: Make it possible to start a new round again
 
         if (gameOver) {
             timer.stop();
             return;
         }
         final List<WarpPipes> toRemove = new ArrayList<>();
-        //Removed by Tommi: for (Rectangle alien : aliens) {
+        // Removed by Tommi: for (Rectangle alien : aliens) {
         for (WarpPipes pipe : pipeList) {
             pipe.translate(-1, 0);
-            
+
             if (pipe.noLongerOnScreen()) {
                 // we add to another list and remove later
                 // to avoid concurrent modification in a for-each loop
@@ -99,18 +109,26 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
             }
 
             // Space ship has crashed into pipe
-            if (pipe.intersects(spaceShip)) {
+            if (pipe.intersects(bird)) {
                 gameOver = true;
             }
         }
-        
+
         pipeList.removeAll(toRemove);
         // add a new pair of Warp Pipes for every one that was removed.
         for (int i = 0; i < toRemove.size(); ++i) {
             Dimension d = getSize();
             pipeList.add(new WarpPipes(d.width, d.height));
-            score++;
-            //addWarpPipe(d.width, d.height);
+
+            // For every pipe that passes the screen, one currentScore is added.
+            // If your current currentScore is higher than your highscore,
+            // the highscore is updated.
+
+            currentScore++;
+            if (highscore < currentScore) {
+                highscore = currentScore;
+            }
+            // addWarpPipe(d.width, d.height);
         }
 
         // jumpRemaining is an instance variable, everytime
@@ -118,24 +136,24 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
         // the bird jumps 5px in Y-direction until jumpRemaining
         // becomes 0. This is because we want the bird to jump
         // in a smooth motion.
-        
+
         if (jumpRemaining > 0) {
-            spaceShip.translate(0, -2);
+            bird.translate(0, -2);
             jumpRemaining = jumpRemaining - 2;
             gravity = 0;
         }
 
         // After every jump, the gravity starts affecting
         // the bird again. Variable gravity is also set to 0 after
-        // every jump. 
+        // every jump.
         // Currently the instance variable gravity increases by 1
-        // every 7th frame. To have less or more gravity, 
-        // change the 7 to any other number. Higher number means 
+        // every 7th frame. To have less or more gravity,
+        // change the 7 to any other number. Higher number means
         // lower gravity and lower number means higher gravity.
 
         else {
-            spaceShip.translate(0, gravity);
-            if(frames % 7 == 0) {
+            bird.translate(0, gravity);
+            if (frames % 7 == 0) {
                 frames = 0;
                 gravity++;
             }
@@ -147,6 +165,7 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
     public void keyReleased(KeyEvent e) {
         //
     }
+
     @Override
     public void keyTyped(KeyEvent e) {
         // do nothing
@@ -156,15 +175,35 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
     public void keyPressed(KeyEvent e) {
         // this event triggers when we press a key
         // in this case Space bar is for jumping
-
-        if (gameOver) {
-            return;
-        }
         final int minHeight = 10;
-        final int maxHeight = this.getSize().height - spaceShip.height - 10;
+        final int maxHeight = this.getSize().height - bird.height - 10;
         final int kc = e.getKeyCode();
-        if (kc == KeyEvent.VK_SPACE && spaceShip.y > minHeight && spaceShip.y < maxHeight) {
-                jumpRemaining = 30;
+
+        if (gameOver && kc == KeyEvent.VK_SPACE) {
+            // When game is over, gameover screen will show and timer will stop
+            // To restart game, click space button. 
+
+            // New bird is drawn and will start at the same position
+            // as when the game started the first time.
+            this.bird = new Rectangle(20, width / 2 - 15, 30, 20);
+
+            // The List of pipes have to be cleared, otherwise
+            // the old pipes from last round will still appear on the screen.
+            // Then add a new pipe again.
+            pipeList.clear();
+            pipeList.add(new WarpPipes(width, height));
+
+            // Clear the current current score
+            currentScore = 0;
+
+            // Set gameOver to false again so that the repaint method will
+            // draw the game and then start the timer again.
+            gameOver = false;
+            timer.start();
+        }
+
+        else if (kc == KeyEvent.VK_SPACE && bird.y > minHeight && bird.y < maxHeight) {
+            jumpRemaining = 30;
         }
     }
 }
