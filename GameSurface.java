54a9cdd590c9c13ce596999a7eef84aa590992b0
AleGab0710 <AleGab0710@users.noreply.github.com>
2021-02-24 15:25:29 +0100

    Refactoring, new animations

--- GameSurface.java
+++ GameSurface.java
@@ -35,266 +35,273 @@ import javax.swing.Timer;
  */
 
 public class GameSurface extends JPanel implements ActionListener, KeyListener {
-    private static final long serialVersionUID = 6260582674762246325L;
+	private static final long serialVersionUID = 6260582674762246325L;
 
-    private boolean gameOver;
-    private Timer timer;
-    private List<Rectangle> pipes;
-    private Rectangle bird;
-    private int yMotion;
-    private final int width = 400;
-    private final int height = 400;
-    private int score = 0;
-   // private int highScore;
-    private TreeMap<Integer, String> highScores = new TreeMap<Integer, String>();
-    private String playerName;
-    private int scoreListCount = 0;
-    Integer fakeScore = 1;
+	private boolean gameOver;
+	private Timer timer;
+	private List<Rectangle> pipes;
+	private Rectangle bird;
+	private int yMotion;
+	private final int width = 400;
+	private final int height = 400;
+	private int score = 0;
+	// private int highScore;
+	private TreeMap<Integer, String> highScores = new TreeMap<Integer, String>();
+	private String playerName;
+	private int scoreListCount = 0;
+	Integer fakeScore = 1;
 
-    public GameSurface() {
-        this.gameOver = false;
-        this.pipes = new ArrayList<>();
+	public Image bg;
+	public BufferedImage go;
+	public BufferedImage pip;
 
-        addPipes(width, height);
+	public GameSurface() {
+		this.gameOver = false;
+		this.pipes = new ArrayList<>();
+
+		addPipes(width, height);
 //        for (int i = 0; i < 10; ++i) {
 //            highScores.put(fakeScore, "Bosse");
 //            fakeScore++;
 //        }
-        this.bird = new Rectangle(width / 3, width / 2, 40, 28);
+		this.bird = new Rectangle(width / 3, width / 2, 40, 28);
 
-        this.timer = new Timer(20, this);
-        this.timer.start();
-    }
+		this.timer = new Timer(20, this);
+		this.timer.start();
+	}
 
-    private void restart() {
-        this.gameOver = false;
-        this.pipes = new ArrayList<>();
+	private void restart() {
+		this.gameOver = false;
+		this.pipes = new ArrayList<>();
 
-        addPipes(width, height);
+		addPipes(width, height);
 
-        this.bird = new Rectangle(width / 3, width / 2, 40, 28);
+		this.bird = new Rectangle(width / 3, width / 2, 40, 28);
 
-        this.timer = new Timer(20, this);
-        this.timer.start();
-    }
+		this.timer = new Timer(20, this);
+		this.timer.start();
+	}
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        try {
-            repaint(g);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		// g.drawImage(go, 80, 100, null);
+		try {
+			repaint(g);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    }
+	}
 
-    private void addPipes(final int width, final int height) {
-        int randomHeight = ThreadLocalRandom.current().nextInt(height / 4, height / 2);
-        int gap = 150;
+	private void addPipes(final int width, final int height) {
+		int randomHeight = ThreadLocalRandom.current().nextInt(height / 4, height / 2);
+		int gap = 150;
 
-        // top pipe
-        pipes.add(new Rectangle(width, 0, 50, (height - (randomHeight + gap))));
-        // console log for bugfixes
-        // System.out.println("top pipe:\t" + "x: " + width + "\ty: " + 0 + "\theight: "
-        // + (height - (randomHeight + gap)));
+		// top pipe
+		pipes.add(new Rectangle(width, 0, 50, (height - (randomHeight + gap))));
+		// console log for bugfixes
+		// System.out.println("top pipe:\t" + "x: " + width + "\ty: " + 0 + "\theight: "
+		// + (height - (randomHeight + gap)));
 
-        // bottom pipe
-        pipes.add(new Rectangle(width, (height - randomHeight), 50, randomHeight));
-        // console log for bugfixes
-        // System.out.println("bottom pipe:\t" + "x:" + width + "\ty:" + (height -
-        // randomHeight) + "\theight:" + randomHeight);
+		// bottom pipe
+		pipes.add(new Rectangle(width, (height - randomHeight), 50, randomHeight));
+		// console log for bugfixes
+		// System.out.println("bottom pipe:\t" + "x:" + width + "\ty:" + (height -
+		// randomHeight) + "\theight:" + randomHeight);
 
-    }
+	}
 
-    /**
-     * Call this method when the graphics needs to be repainted on the graphics
-     * surface.
-     * 
-     * @param g the graphics to paint on
-     * @throws IOException
-     */
-    private void repaint(Graphics g) throws IOException {
-        final Dimension d = this.getSize();
+	/**
+	 * Call this method when the graphics needs to be repainted on the graphics
+	 * surface.
+	 * 
+	 * @param g the graphics to paint on
+	 * @throws IOException
+	 */
+	private void repaint(Graphics g) throws IOException {
+		final Dimension d = this.getSize();
+		go = ImageIO.read(Path.of("images/gameOver.png").toFile());
 
-        if (gameOver) {
-            if (isItHighscore(score)) {
+		if (gameOver) {
+			g.drawImage(go, 75, 100, null);
+			if (isItHighscore(score)) {
 
-                JDialog da = new JDialog();
-                da.setBackground(Color.green);
-                da.setLocation(height / 2, width / 2);
-                da.setVisible(true);
-                playerName = JOptionPane.showInputDialog(da, "Highscore! Write your name:");
-                highScores.put(score, playerName);
-                da.setVisible(false);
+				JDialog da = new JDialog();
+				da.setBackground(Color.green);
+				da.setLocation(height / 2, width / 2);
+				da.setVisible(true);
+				playerName = JOptionPane.showInputDialog(da, "Highscore! Write your name:");
+				highScores.put(score, playerName);
+				da.setVisible(false);
 
-               
-            }
+			}
 
-            g.setColor(Color.red);
-            g.fillRect(0, 0, d.width, d.height);
-            g.setColor(Color.black);
-            g.setFont(new Font("Arial", Font.BOLD, 48));
-            g.drawString("Game Over!", d.height / 6, d.width / 6);
-            
-            g.setFont(new Font("Arial", Font.BOLD, 20));
-            g.drawString(toString(), d.height / 3, ((d.width / 4)));
-            
-            g.setFont(new Font("Arial", Font.BOLD, 20));
-            g.drawString("Highscores:", d.height / 5, ((d.width / 3)));
-            
-            for (Map.Entry<Integer, String> entry : highScores.entrySet()) {
+//            g.setColor(Color.red);
+//            g.fillRect(0, 0, d.width, d.height);
+//            g.setColor(Color.black);
+//            g.setFont(new Font("Arial", Font.BOLD, 48));
+//            g.drawString("Game Over!", d.height / 6, d.width / 6);
 
-                g.setFont(new Font("Arial", Font.BOLD, 20));
-                g.drawString(entry.getKey().toString() + "   " + 
-                entry.getValue().toString(), (d.height / 2) + 20, ((d.width / 3) + 
-                        scoreListCount));
-                System.out.println(entry.getKey()+ entry.getValue());
-                score = 0;
-                scoreListCount += 20;
-            }
-            scoreListCount =  0;
-            return;
-        }
+			g.setFont(new Font("Arial", Font.BOLD, 20));
+			g.drawString(toString(), d.height / 3, ((d.width / 4)));
 
-        // fill the background
-        g.setColor(Color.cyan);
-        g.fillRect(0, 0, d.width, d.height);
+			g.setFont(new Font("Arial", Font.BOLD, 20));
+			g.drawString("Highscores:", d.height / 5, ((d.width / 3)));
 
-        // draw the pipes
-        for (Rectangle pipe : pipes) {
-            g.setColor(Color.green);
-            g.fillRect(pipe.x, pipe.y, pipe.width, pipe.height);
-        }
+			for (Map.Entry<Integer, String> entry : highScores.entrySet()) {
 
-        // draw the space ship
-        Image img = ImageIO.read(Path.of("images/Bird2.png").toFile());
-        g.drawImage(img, bird.x, bird.y, null);
-        BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-        Graphics2D bGr = bimage.createGraphics();
-        bGr.drawImage(bimage, 40, 28, null);
-        bGr.dispose();
-    }
+				g.setFont(new Font("Arial", Font.BOLD, 20));
+				g.drawString(entry.getKey().toString() + "   " + entry.getValue().toString(), (d.height / 2) + 20,
+						((d.width / 3) + scoreListCount));
+				System.out.println(entry.getKey() + entry.getValue());
+				score = 0;
+				scoreListCount += 20;
+			}
+			scoreListCount = 0;
+			return;
+		}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // this will trigger on the timer event
-        // if the game is not over yet it will
-        // update the positions of all pipes
-        // and check for collision with the space ship
+		// fill the background
+		BufferedImage bg;
+		bg = ImageIO.read(Path.of("images/bluemoon.png").toFile());
+		g.drawImage(bg, 0, 0, null);
 
-        if (gameOver) {
-            timer.stop();
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-            return;
-        }
+		// draw the pipes
+		pip = ImageIO.read(Path.of("images/redPipe.png").toFile());
+		for (Rectangle pipe : pipes) {
+			g.drawImage(pip, pipe.x, pipe.y, pipe.width, pipe.height, null);
+		}
 
-        final List<Rectangle> toRemove = new ArrayList<>();
+		// draw the bird
+		Image img = ImageIO.read(Path.of("images/GBirdUp.png").toFile());
+		g.drawImage(img, bird.x, bird.y, null);
+		BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		Graphics2D bGr = bimage.createGraphics();
+		bGr.drawImage(bimage, 40, 28, null);
+		bGr.dispose();
 
-        for (Rectangle pipe : pipes) {
-            pipe.translate(-1, 0);
-            if (pipe.x + pipe.width < 0) {
-                // we add to another list and remove later
-                // to avoid concurrent modification in a for-each loop
-                toRemove.add(pipe);
-            }
+	}
 
-            if (pipe.intersects(bird)) {
-                gameOver = true;
-            }
-        }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// this will trigger on the timer event
+		// if the game is not over yet it will
+		// update the positions of all pipes
+		// and check for collision with the space ship
 
-        pipes.removeAll(toRemove);
+		if (gameOver) {
+			timer.stop();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return;
+		}
 
-        // add new pipes for every one that was removed
-        for (int i = 0; i < toRemove.size(); ++i) {
-            // Dimension d = getSize();
-            addPipes(width, height);
-        }
+		final List<Rectangle> toRemove = new ArrayList<>();
 
-        this.repaint();
+		for (Rectangle pipe : pipes) {
+			pipe.translate(-1, 0);
+			if (pipe.x + pipe.width < 0) {
+				// we add to another list and remove later
+				// to avoid concurrent modification in a for-each loop
+				toRemove.add(pipe);
+			}
 
-        bird.y -= yMotion;
+			if (pipe.intersects(bird)) {
+				gameOver = true;
+			}
+		}
 
-        if (bird.y < 0 || bird.y > height) {
-            gameOver = true;
-        }
+		pipes.removeAll(toRemove);
 
-        // Awards one point if bird passes trough a set of pipes
-        if (pipes.get(0).x == (width / 3 - bird.width) && !gameOver) {
-            score++;
-            System.out.println("Current score: " + score);
-        }
+		// add new pipes for every one that was removed
+		for (int i = 0; i < toRemove.size(); ++i) {
+			// Dimension d = getSize();
+			addPipes(width, height);
+		}
 
-    }
+		this.repaint();
 
-    public boolean isItHighscore(int score) {
-        // highScores.put("lena", 0);
+		bird.y -= yMotion;
 
-        if (highScores.size() <= 10) {
-            return true;
-        }
+		if (bird.y < 0 || bird.y > height) {
+			gameOver = true;
+		}
 
-        if (highScores.size() >= 10) {
+		// Awards one point if bird passes trough a set of pipes
+		if (pipes.get(0).x == (width / 3 - bird.width) && !gameOver) {
+			score++;
+			System.out.println("Current score: " + score);
+		}
 
-            if (highScores.firstKey() < score) {
+	}
 
-                highScores.remove(highScores.firstEntry());
+	public boolean isItHighscore(int score) {
+		// highScores.put("lena", 0);
 
-                return true;
-            }
-        }
+		if (highScores.size() <= 10) {
+			return true;
+		}
 
-        return false;
-    }
+		if (highScores.size() >= 10) {
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // this event triggers when we release a key and then
-        // we will move the space ship if the game is not over yet
+			if (highScores.firstKey() < score) {
 
-        final int maxHeight = this.getSize().height - bird.height - 10;
-        final int kc = e.getKeyCode();
+				highScores.remove(highScores.firstEntry());
 
-        if (!gameOver && kc == KeyEvent.VK_SPACE && bird.y < maxHeight) {
-            jump();
-        } else if (gameOver && kc == KeyEvent.VK_SPACE) {
-            restart();
-        }
-    }
+				return true;
+			}
+		}
 
-    public int getScore() {
-        return score;
-    }
+		return false;
+	}
 
-    @Override
-    public String toString() {
-        return "You scored: " + score;
-    }
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// this event triggers when we release a key and then
+		// we will move the space ship if the game is not over yet
 
-    public void jump() {
-        bird.translate(0, -50);
-        if (yMotion < 0) {
-            yMotion = 0;
-        }
-        yMotion -= 3;
+		final int maxHeight = this.getSize().height - bird.height - 10;
+		final int kc = e.getKeyCode();
 
-    }
+		if (!gameOver && kc == KeyEvent.VK_SPACE && bird.y < maxHeight) {
+			jump();
+		} else if (gameOver && kc == KeyEvent.VK_SPACE) {
+			restart();
+		}
+	}
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // do nothing
-    }
+	public int getScore() {
+		return score;
+	}
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        // do nothing
-    }
+	@Override
+	public String toString() {
+		return "You scored: " + score;
+	}
+
+	public void jump() {
+		bird.translate(0, -50);
+		if (yMotion < 0) {
+			yMotion = 0;
+		}
+		yMotion -= 3;
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// do nothing
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// do nothing
+	}
 
 }
