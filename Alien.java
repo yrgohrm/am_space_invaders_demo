3b156a349d320aa95ca4981e68cb628ce8bdba83
Hampus Ram <hampus.ram@educ.goteborg.se>
2022-01-24 11:56:31 +0100

    Alien does not need to be serializable

--- Alien.java
+++ Alien.java
@@ -1,11 +1,8 @@
 import java.awt.Rectangle;
-import java.io.Serializable;
 
 // this class can be much improved, better encapsulation
 // draw itself, update itself etc. etc.
-public class Alien implements Serializable {
-    private static final long serialVersionUID = -6385903094117229614L;
-
+public class Alien {
     public final int created;
     public final Rectangle bounds;
 
